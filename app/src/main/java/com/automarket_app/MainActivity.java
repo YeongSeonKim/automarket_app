c772bab353cc83d0c7f8a9541c009c1bc4b776d0
Yeongseon Kim <son960311@naver.com>
2019-09-17 16:11:12 +0900

    Add Intro Activity

--- app/src/main/java/com/automarket_app/MainActivity.java
+++ app/src/main/java/com/automarket_app/MainActivity.java
@@ -3,12 +3,15 @@ package com.automarket_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 상태바 없애기
+        // getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 }
