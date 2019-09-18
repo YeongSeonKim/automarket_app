ca7cff79a7dbe6ea7579b338857cad742a83776e
Yeongseon Kim <son960311@naver.com>
2019-09-18 17:49:37 +0900

    로그인(login), 회원가입(register) 추가, xml 화면구성

--- app/src/main/java/com/automarket_app/IntroActivity.java
+++ app/src/main/java/com/automarket_app/IntroActivity.java
@@ -14,7 +14,7 @@ public class IntroActivity extends AppCompatActivity {
         @Override
         public void run() {
 // 4초뒤에 다음화면(MainActivity)으로 넘어가기 Handler 사용
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
             startActivity(intent); // 다음화면으로 넘어가기
             finish(); // Activity 화면 제거
         }
