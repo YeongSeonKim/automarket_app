5a7ffac02a839d3e1dd06dabd191c0220d7b8513
Yeongseon Kim <son960311@naver.com>
2019-09-27 17:52:20 +0900

    modify Order

--- app/src/main/java/com/automarket_app/OrderActivity.java
+++ app/src/main/java/com/automarket_app/OrderActivity.java
@@ -17,6 +17,7 @@ import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.TextView;
 
+import com.automarket_app.VO.ProdListVO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.BufferedReader;
@@ -25,78 +26,13 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
 
-//class ProdRunnable implements Runnable {
-//
-//    private String keyword;
-//    private Handler handler;
-//
-//    ProdRunnable(Handler handler, String keyword) {
-//        this.handler = handler;
-//        this.keyword = keyword;
-//    }
-//
-//    @Override
-//    public void run() {
-//        // 구현내용
-//
-//        // keyword를 이용해서 web program에 접속한 후 결과를 받아온다!!
-//        // 결과로 받아온 JSON 문자열을 이용해서 GridView에 출력해야 한다!!
-//        // 외부THread이기떄문에 여기서는 GridView를 제어할 수 없다!!
-//        // 그래서 Handler를 이용해서 UI Thread에 GridView에 사용할 데이터를 넘긴다.
-//
-//        // 네크워트 연결 , localhost를 사용하면 안되고 내가 접속할 실제 웹서버의 ip주소를 입력하면 된다.
-//
-//        // http://localhost:8080/automarket/api/prod/list.do?cid=1
-//
-//        String url = R.string.Server_URL + "/automarket/api/prod/list.do?cid=" + keyword;
-//
-//
-//        // 네트워크로 연결되는 것들은 반드시 예외처리를 해줘야함
-//        try {
-//            URL urlObj = new URL(url); // 주소를 자바 객체로 만들어줄꺼임 -> open connection 사용가능
-//            HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
-//            //연결객체를 만든 다음에 데이터를 읽어드릴수  있는 Stream(통로)을 뽑아낼수 있다.
-//            // network연결이 성공 한 후 데이터를 읽어들이기 위한 데이터 연결 통로 Stream을 생성한다.
-//            BufferedReader br = new BufferedReader(
-//                    new InputStreamReader(con.getInputStream())); // Stream을 더좋은걸로 바꿔주는거
-//
-//            // 데이터 저장 용도로 만듬
-//            String input = "";
-//            StringBuffer sb = new StringBuffer();
-//
-//            // 서버가 보내준 데이터를 읽어서 input에 넣는데 null이 아닐때까지 모두 읽으라는 의미
-//            while ((input = br.readLine()) != null) {
-//                sb.append(input);
-//            }
-//            // Log창에 찍어보기
-//            Log.i("DATA",sb.toString());
-//
-//            // 얻어온 결과 JSON 문자열을 Jackson Library를 이용해서
-//            // Java 객체 형태(String[])로 변형해서 List에 입력할수 있게끔
-//            ObjectMapper mapper = new ObjectMapper();
-//            // Jackson Library를 이용하여 JSON문자열을 String[] 형태로 변환
-//            String[] resultArr = mapper.readValue(sb.toString(), String[].class); // 배열로 변환시켜주기.
-//            // 원래형태 <-> JSON
-//
-//            // 데이터를 합치기 위해서 , 바구니라고 생각하면됨
-//            Bundle bundle = new Bundle();
-//            bundle.putStringArray("PRODARRAY", resultArr); // key, value 쌍으로
-//
-//            // 번들을 보내기 위해서는 메세지가 필요함
-//            Message msg = new Message();
-//            msg.setData(bundle);
-//
-//            // 위에서 만든 메세지 객체를 보냄 , Activtiy에 있는 handler가 받음
-//
-//            handler.sendMessage(msg);
-//
-//
-//        } catch (Exception e) {
-//            Log.i("DATAerror", e.toString());
-//        }
-//    }
-//}
-    public class OrderActivity extends AppCompatActivity {
+ public class OrderActivity extends AppCompatActivity {
+
+
+
+
+        ///////////////////////////////////////////////////////////////////////////////////
+
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -136,10 +72,11 @@ import java.util.List;
                 }
             });
 
-                    ///////////////////////////////////////////////////////////////////////////////////
+            ///////////////////////////////////////////////////////////////////////////////////
+
+
 
             ListView listView1 = (ListView)findViewById(R.id.listView1);
-
 
         // TabHost
 
@@ -178,9 +115,7 @@ import java.util.List;
             tabHost.addTab(tabHost.newTabSpec("6").setContent(R.id.content3).setIndicator("유제품"));
 //            TabHost.TabSpec tab6 = tabHost.newTabSpec("6").setContent(R.id.content6).setIndicator("유제품");
 //            tabHost.addTab(tab6);
+
+
         }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
     }
-}
