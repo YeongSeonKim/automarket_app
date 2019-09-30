4fff5282c46cca0fb4d8272c0eafaeba80f82771
Yeongseon Kim <son960311@naver.com>
2019-09-30 15:23:05 +0900

    modify cash & cart

--- app/src/main/java/com/automarket_app/VO/cashAddVO.java
+++ app/src/main/java/com/automarket_app/VO/cashAddVO.java
@@ -2,4 +2,50 @@ package com.automarket_app.VO;
 
 // 캐시충전
 public class cashAddVO {
+
+    private String userId;   // 사용자 아이디
+    private int chargePrice; // 충전금액
+    private  int balance;    // 잔액
+
+    public cashAddVO() {
+    }
+
+    public cashAddVO(String userId, int chargePrice, int balance) {
+        this.userId = userId;
+        this.chargePrice = chargePrice;
+        this.balance = balance;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public int getChargePrice() {
+        return chargePrice;
+    }
+
+    public void setChargePrice(int chargePrice) {
+        this.chargePrice = chargePrice;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "cashAddVO{" +
+                "userId='" + userId + '\'' +
+                ", chargePrice=" + chargePrice +
+                ", balance=" + balance +
+                '}';
+    }
 }
