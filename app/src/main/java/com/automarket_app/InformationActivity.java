39eb2bed79943bc61e36604673b104f407b58b62
Yeongseon Kim <son960311@naver.com>
2019-09-30 17:55:46 +0900

    Add gitIgnore & modify information

--- app/src/main/java/com/automarket_app/InformationActivity.java
+++ app/src/main/java/com/automarket_app/InformationActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class InformationActivity extends AppCompatActivity {
 
@@ -30,5 +32,11 @@ public class InformationActivity extends AppCompatActivity {
                //finish();
             }
         });
+
+        TextView nowEmail = (TextView)findViewById(R.id.nowEmail);
+
+
+        Button btnLogout = (Button)findViewById(R.id.btnLogout);
+
     }
 }
