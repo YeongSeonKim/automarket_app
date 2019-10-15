c97d8cae468e1ed6cc63e1e72b9c2d6f30c4769c
Yeongseon Kim <son960311@naver.com>
2019-10-15 17:00:49 +0900

    내정보 - 아이디 내정보에 띄우기, 로그아웃하면 로그인페이지로 넘어가기 구현( SharePre에서 삭제)

--- app/src/main/java/com/automarket_app/InformationActivity.java
+++ app/src/main/java/com/automarket_app/InformationActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -43,20 +45,41 @@ public class InformationActivity extends AppCompatActivity {
         appData = getSharedPreferences("login_info",MODE_PRIVATE);
         login_email = appData.getString("EMAIL","");
 
+        Log.i("automarket_app" ,"appData : " + appData);
         Log.i("automarket_app","login_email : " + login_email);
 
         TextView nowEmail = (TextView)findViewById(R.id.nowEmail);
 
+        // 로그인 아이디(이메일)
         nowEmail.setText(login_email);
 
         Log.i("automarket_app","login_email : " + nowEmail);
 
+        // btnLogout - 로그아웃
         Button btnLogout = (Button)findViewById(R.id.btnLogout);
+
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // value 값을 null로 할당
+//                appData.getStringSet("USERID",null);
+//                Log.i("automarket_app" ,"appData : " + appData);
+//
+//                Intent intent = new Intent(InformationActivity.this, LoginActivity.class);
+//                startActivity(intent);
 
+                // 사용자를 아예 지워버리는거
+               appData.edit().clear().commit();
+
+                Log.i("automarket_app_appdata" ,"appData 삭제 : " + appData);
+
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                startActivity(intent);
 
             }
         });
