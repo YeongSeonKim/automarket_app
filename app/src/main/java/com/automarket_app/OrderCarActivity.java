9b7d482cc87873bcf145247d6a86dc52940d7f46
kyunghee2 <kyungh103@gmail.com>
2019-10-17 10:41:48 +0900

    수정

--- app/src/main/java/com/automarket_app/OrderCarActivity.java
+++ app/src/main/java/com/automarket_app/OrderCarActivity.java
@@ -324,7 +324,7 @@ public class OrderCarActivity extends AppCompatActivity  implements MapView.Curr
                 startService(i);
 
                 Toast.makeText(OrderCarActivity.this,"주문처리 되었습니다.",Toast.LENGTH_LONG).show();
-                Intent i_order = new Intent(getApplicationContext(), OrderActivity.class);
+                Intent i_order = new Intent(getApplicationContext(), InformationActivity.class);
 
                 startActivity(i_order);
 
